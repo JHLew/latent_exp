da6e5a61b9aed2059649ed03c4321ea266ab6efb
JHLew <fudojhl@gmail.com>
2021-08-12 18:11:48 +0900

    dataloader drafted

--- data.py
+++ data.py
@@ -2,6 +2,8 @@ from torch.utils.data import Dataset
 from glob import glob
 from PIL import Image
 from torchvision.transforms.functional import to_tensor
+from random import randint
+
 
 class _Dataset(Dataset):
     def __init__(self, path, resolution=None, is_train=True):
@@ -22,9 +24,39 @@ class _Dataset(Dataset):
         img = Image.open(img)
 
         # data augmentation
+        if self.is_train:
+            w, h = img.size
+            # cropping
+            # choose size
+            w_size, h_size = randint(self.resolution, w), randint(self.resolution, h)
+
+            # choose region
+            w_range, h_range = w - w_size, h - h_size
+            w_start, h_start = randint(0, w_range), randint(0, h_range)
+            w_end, h_end = w_start + w_range, h_start + h_range
+
+            # crop
+            augmented_img = img.crop((w_start, w_end, h_start, h_end))
+
+            # resize
+            augmented_img = augmented_img.resize((self.resolution, self.resolution))
+
+            # flip
+            flip_flag = randint(0, 1)
+            if flip_flag == 1:
+                augmented_img = augmented_img.transpose(Image.FLIP_LEFT_RIGHT)
+
+            # rotate
+            rotate_flag = randint(0, 3)
+            if rotate_flag == 1:
+                augmented_img = augmented_img.rotate(Image.ROTATE_90)
+            elif rotate_flag == 2:
+                augmented_img = augmented_img.rotate(Image.ROTATE_180)
+            elif rotate_flag == 3:
+                augmented_img = augmented_img.rotate(Image.ROTATE_270)
+
+            img = augmented_img
 
         img = to_tensor(img)
 
         return img
-
-
